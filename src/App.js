4622e21058f210faa1f582199fba007a0fdb4afb
Pobarul-islam <pobarul3@gmail.com>
2022-03-28 15:27:09 +0600

    some code added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-import CardGroup from './components/CardGroup/CardGroup';
+import logo from "./logo.svg";
+import "./App.css";
+import CardGroup from "./components/CardGroup/CardGroup";
 
 function App() {
   return (
     <div className="App">
-     <CardGroup></CardGroup>
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
+      <button type="button" className="btn btn-secondary">
+        Secondary
+      </button>
+      <button type="button" className="btn btn-success">
+        Success
+      </button>
+      <button type="button" className="btn btn-danger">
+        Danger
+      </button>
+      <button type="button" className="btn btn-warning">
+        Warning
+      </button>
+      <button type="button" className="btn btn-info">
+        Info
+      </button>
+      <button type="button" className="btn btn-light">
+        Light
+      </button>
+      <button type="button" className="btn btn-dark">
+        Dark
+      </button>
+
+      <button type="button" className="btn btn-link">
+        Link
+      </button>
+      <CardGroup></CardGroup>
     </div>
   );
 }
--- src/components/CardGroup/CardGroup.js
+++ src/components/CardGroup/CardGroup.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import Card from "../Card/Card";
 
 const CardGroup = () => {
-    return (
-        <div>
-            <h2>This is my cards</h2>
-        </div>
-    );
+    const products = [
+        {id:1, name: 'laptop apple', price: 125000},
+        {id:2, name: 'laptop walton', price: 30000},
+        {id:3, name: 'laptop dell', price: 30400},
+        {id:4, name: 'laptop hp', price: 948343}
+    ]
+  return (
+    <div>
+      <h2>This is my cards</h2>
+          <div className="card-group">
+              {
+                  products.map(product => <Card key={product.id} product={product}></Card>)
+              }
+              
+      </div>
+    </div>
+  );
 };
 
 export default CardGroup;
